7f3de381142f50109d87ab9c8bf5f0c3f81be3b2
Yan Logan <yanlogans@gmail.com>
2025-07-09 03:18:07 +0300

    feat: add Octave interface, OctaveName and OctaveLabel types

--- src/types/pianoKeys.ts
+++ src/types/pianoKeys.ts
@@ -4,6 +4,15 @@ export type KeyName = 'C' | 'CisDes' | 'D' | 'DisEs' | 'E' | 'F' | 'FisGes' | 'G
 
 export type KeyLabel = 'C' | 'C# / D♭' | 'D' | 'D# / E♭' | 'E' | 'F' | 'F# / G♭' | 'G' | 'G# / A♭' | 'A' | 'A# / B♭' | 'H';
 
+export type OctaveName = 'great' | 'small' | 'one-line' | 'two-line';
+
+export type OctaveLabel = 'Большая' | 'Малая' | 'Первая' | 'Вторая';
+
+export interface Octave {
+    octaveName: OctaveName;
+    octaveLabel: OctaveLabel
+}
+
 export interface OctaveNote {
     pianoKey: KeyName;
     pianoKeyLabel: KeyLabel;
