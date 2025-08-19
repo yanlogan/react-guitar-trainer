1f67f65c29368c6ca89824397fc830f36bca101f
Yan Logan <yanlogans@gmail.com>
2025-08-19 19:39:48 +0300

    feat: add handleKeyClick for PianoKey

--- src/components/PianoKey/PianoKey.tsx
+++ src/components/PianoKey/PianoKey.tsx
@@ -1,5 +1,6 @@
 import "./PianoKey.scss";
 import type {KeyColor, KeyLabel, KeyName, OctaveName} from "../../types/pianoKeys";
+import {useState} from "react";
 
 interface PianoKeyProps {
     pianoKey: KeyName,
@@ -10,12 +11,28 @@ interface PianoKeyProps {
 }
 
 const PianoKey = ({pianoKey, octaveName, pianoKeyLabel, color, showLabel = false}: PianoKeyProps) => {
+    const [active, setActive] = useState(false);
+
+    const handleKeyClick = () => {
+        setActive(true);
+        const audio = document.getElementById(`piano_${octaveName}_${pianoKey}`) as HTMLAudioElement;
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+        setTimeout(() => {
+            setActive(false);
+        }, 500);
+    }
+
     return (
-        <div className={`PianoKey PianoKey--${color}`} data-piano-key={`piano_${octaveName}_${pianoKey}`}>
-            {showLabel ? (
-                <span className="PianoKey__label">{pianoKeyLabel}</span>
-            ) : null}
-        </div>
+        <>
+            <audio id={`piano_${octaveName}_${pianoKey}`} src={`./audio/piano/piano_${octaveName}_${pianoKey}.mp3`}></audio>
+            <div className={`PianoKey PianoKey--${color}${active ? ' PianoKey--active' : ''}`} data-piano-key={`piano_${octaveName}_${pianoKey}`} onClick={handleKeyClick}>
+                {showLabel ? (
+                    <span className="PianoKey__label">{pianoKeyLabel}</span>
+                ) : null}
+            </div>
+        </>
     );
 }
 
