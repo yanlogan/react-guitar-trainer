6f662867e38151035a7ba20d3b42da0a9dea1828
Yan Logan <yanlogans@gmail.com>
2025-08-19 19:53:13 +0300

    refactor: switch getElementById to useRef in PianoKey

--- src/components/PianoKey/PianoKey.tsx
+++ src/components/PianoKey/PianoKey.tsx
@@ -1,6 +1,6 @@
 import "./PianoKey.scss";
 import type {KeyColor, KeyLabel, KeyName, OctaveName} from "../../types/pianoKeys";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 interface PianoKeyProps {
     pianoKey: KeyName,
@@ -12,13 +12,16 @@ interface PianoKeyProps {
 
 const PianoKey = ({pianoKey, octaveName, pianoKeyLabel, color, showLabel = false}: PianoKeyProps) => {
     const [active, setActive] = useState(false);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
     const handleKeyClick = () => {
+        if (!audioRef.current) return;
+
         setActive(true);
-        const audio = document.getElementById(`piano_${octaveName}_${pianoKey}`) as HTMLAudioElement;
-        audio.pause();
-        audio.currentTime = 0;
-        audio.play();
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+
         setTimeout(() => {
             setActive(false);
         }, 500);
@@ -26,7 +29,7 @@ const PianoKey = ({pianoKey, octaveName, pianoKeyLabel, color, showLabel = false
 
     return (
         <>
-            <audio id={`piano_${octaveName}_${pianoKey}`} src={`./audio/piano/piano_${octaveName}_${pianoKey}.mp3`}></audio>
+            <audio id={`piano_${octaveName}_${pianoKey}`} src={`./audio/piano/piano_${octaveName}_${pianoKey}.mp3`} ref={audioRef}></audio>
             <div className={`PianoKey PianoKey--${color}${active ? ' PianoKey--active' : ''}`} data-piano-key={`piano_${octaveName}_${pianoKey}`} onClick={handleKeyClick}>
                 {showLabel ? (
                     <span className="PianoKey__label">{pianoKeyLabel}</span>
